1c2d73f95b5b4437911950206f64fbf514058cff
Sourav Dhillon <souravdhillon50@gmail.com>
2025-06-26 20:12:29 +0530

    successfully deployed todo app

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/to_do_list_using-react/',
+   base: '/to_do_list_using_react/',
   plugins: [react(),
       tailwindcss()
   ],
